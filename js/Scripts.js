8d0ee3bb3d0a634d185416addf20815949a1d703
SergioAguirreUPTC <sergio.aguirre@uptc.edu.co>
2022-07-21 23:39:53 -0500

    Commit 43 Sergio

--- js/Scripts.js
+++ js/Scripts.js
@@ -6,10 +6,26 @@ var popupRegistro = document.getElementById("popupRegistro");
 var overlay = document.getElementById("overlay");
 var popup = document.getElementById("popup");
 
-const btnCerrarPopupActividad = document.getElementById("btn-cerrar-popupActivity");
-btnCerrarPopupActividad.addEventListener('click', function (){
-    overlayActividad.classList.remove('active');
-    popupActividad.classList.remove('active');
-    document.getElementById("noteActivity").disabled=true;
-    document.getElementById("btn-agregar-calificacion").disabled=true;
+const btnCerrarPopupRecord = document.getElementById("btn-cerrar-popupRecord");
+btnCerrarPopupRecord.addEventListener('click', function (){
+    overlay.classList.remove('active');
+    popup.classList.remove('active');
 });
+
+function agregarRegistro(){
+    overlay.classList.add('active');
+    popup.classList.add('active');
+    overlayRegistro.classList.remove('active');
+    popupRegistro.classList.remove('active');
+}
+
+function cancelarRegistro(){
+    overlay.classList.remove('active');
+    popup.classList.remove('active');
+    overlayRegistro.classList.add('active');
+    popupRegistro.classList.add('active');
+    document.getElementById("newActivityDate").value = "";
+    document.getElementById("newRecordHour").value = "";
+    document.getElementById("newRecordAula").value = "";
+    document.getElementById("newRecordMatter").value = "";
+}
